14a1978c33e80d22753cd4999215bc1912b9e321
Joshua Lehman <joshlehman.dev@gmail.com>
2023-07-05 15:41:01 -0400

    texts_complete

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -9,4 +9,13 @@ export const handlers = [
       ])
     );
   }),
+  rest.get("http://localhost:3030/toppings", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: "Cherrie's", imagePath: "/images/chesrries.png" },
+        { name: "M&M's", imagePath: "/images/m-and-ms.png" },
+        { name: "Hot fudge", imagePath: "/images/hot-fudge.png" },
+      ])
+    );
+  }),
 ];
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -13,3 +13,21 @@ test("displays image for each scoop from server", async () => {
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("displays image for each topping from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  //find images
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /toppings/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  //confirm alt text images
+  const altText = toppingImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    "Cherrie's toppings",
+    "M&M's toppings",
+    "Hot fudge toppings",
+  ]);
+});
