68a6102f7db5c704c7a33aacb1b8986378ece1c6
Joshua Lehman <joshlehman.dev@gmail.com>
2023-07-05 15:41:19 -0400

    url_fix

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -12,7 +12,7 @@ export const handlers = [
   rest.get("http://localhost:3030/toppings", (req, res, ctx) => {
     return res(
       ctx.json([
-        { name: "Cherrie's", imagePath: "/images/chesrries.png" },
+        { name: "Cherrie's", imagePath: "/images/cherries.png" },
         { name: "M&M's", imagePath: "/images/m-and-ms.png" },
         { name: "Hot fudge", imagePath: "/images/hot-fudge.png" },
       ])
