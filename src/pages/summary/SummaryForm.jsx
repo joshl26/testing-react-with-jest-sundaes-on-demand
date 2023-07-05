6c2f67feb22f5384a73ce5c10350b7a99f8cbf51
Joshua Lehman <joshlehman.dev@gmail.com>
2023-07-05 00:05:59 -0400

    complete

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
 
 const SummaryForm = () => {
   const [checked, setChecked] = useState(false);
 
   console.log(checked);
 
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>no ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
       I agree to
-      <span style={{ color: "blue" }}>Terms and conditions</span>
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}>Terms and conditions</span>
+      </OverlayTrigger>
     </span>
   );
 
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -31,3 +31,24 @@ test("Checking checkbox enables and disables button", async () => {
   await user.click(checkbox);
   expect(button).toBeDisabled();
 });
+
+test("Popover responds to hover", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  //popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  //popover appears on mouseover of checkbox
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  //popover dissappears when we mouse out of checkbox
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
+});
