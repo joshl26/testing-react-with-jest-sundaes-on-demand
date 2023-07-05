d9a21a34a4eda8e853ee8738436324e6a2543a74
Joshua Lehman <joshlehman.dev@gmail.com>
2023-07-05 15:05:12 -0400

    react_scripts_fix

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
+import Row from "react-bootstrap/Row";
 import ScoopOption from "./ScoopOption";
 
-const Options = ({ optionType }) => {
+export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
 
-  //optionType is "scoops" or "toppings"
+  // optionType is 'scoops' or 'toppings
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
       .catch((error) => {
-        //TODO: handle error response
+        // TODO: handle error response
       });
   }, [optionType]);
 
-  //TODO: //replace null with ToppingOption when available
+  // TODO: replace `null` with ToppingOption when available
   const ItemComponent = optionType === "scoops" ? ScoopOption : null;
 
   const optionItems = items.map((item) => (
@@ -26,7 +27,5 @@ const Options = ({ optionType }) => {
     />
   ));
 
-  return <div>{optionItems}</div>;
-};
-
-export default Options;
+  return <Row>{optionItems}</Row>;
+}
