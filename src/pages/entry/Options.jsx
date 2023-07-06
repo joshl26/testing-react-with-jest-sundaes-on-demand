ce2e9f3be5f1e5fcb76be2fe605167a2478f212d
Joshua Lehman <joshlehman.dev@gmail.com>
2023-07-05 23:20:43 -0400

    errors

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -5,10 +5,13 @@ import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 import AlertBanner from "../common/AlertBanner";
 import { pricePerItem } from "../../constants";
+import { formatCurrency } from "../../utilities";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const { totals } = useOrderDetails();
 
   // optionType is 'scoops' or 'toppings
   useEffect(() => {
@@ -39,7 +42,10 @@ export default function Options({ optionType }) {
   return (
     <>
       <h2>{title}</h2>
-      <p>{pricePerItem[optionType]} each</p>
+      <p>{formatCurrency(pricePerItem[optionType])} each</p>
+      <p>
+        {title} total: {formatCurrency(totals[optionType])}
+      </p>
       <Row>{optionItems}</Row>
     </>
   );
--- src/pages/summary/OrderSummary.jsx
+++ src/pages/summary/OrderSummary.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import SummaryForm from "./SummaryForm";
+import { useOrderDetails } from "../../contexts/OrderDetails";
+import { formatCurrency } from "../../utilities";
 
 const OrderSummary = () => {
-  return (
-    <div>OrderSummary</div>
-  )
-}
+  const { totals, optionCounts } = useOrderDetails();
 
-export default OrderSummary
+  const scoopArray = Object.entries(optionCounts.scoops); // [["chocolate", 2], ["vanilla", 1]]
+  const scoopList = scoopArray.map(([key, value]) => (
+    <li key={key}>
+      {value} {key}
+    </li>
+  ));
+
+  const toppingArray = Object.keys(optionCounts.toppings); //["M&M's", "Gummi Bears"]
+  const toppingList = toppingArray.map((key) => <li key={key}>{key}</li>);
+
+  return (
+    <div>
+      <h1>Order Summary</h1>
+      <h2>Scoops: {formatCurrency(totals.scoops)}</h2>
+      <ul>{scoopList}</ul>
+      <h2>Toppings: {formatCurrency(totals.toppings)}</h2>
+      <ul>{toppingList}</ul>
+      <SummaryForm />
+    </div>
+  );
+};
+
+export default OrderSummary;
