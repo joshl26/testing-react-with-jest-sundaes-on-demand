8e5c5ee81cd15bef895042607f9394e0fd005bbf
Joshua Lehman <joshlehman.dev@gmail.com>
2023-07-08 23:35:35 -0400

    sundaes_on_demand_start

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
-import { useState } from "react";
-
-export function replaceCamelCaseWithSpaces(colorName) {
-  return colorName.replace(/\B([A-Z])\B/g, " $1");
-}
+import "./App.css";
+import SummaryForm from "./pages/summary/SummaryForm";
 
 function App() {
-  const [buttonColor, setButtonColor] = useState("midnightblue");
-  const [boxChecked, setBoxChecked] = useState(false);
-  const newButtonColor =
-    buttonColor === "mediumvioletred" ? "midnightblue" : "mediumvioletred";
-
   return (
-    <div>
-      <button
-        disabled={boxChecked}
-        style={{
-          backgroundColor: boxChecked ? "grey" : newButtonColor,
-          color: "white",
-        }}
-        onClick={() => {
-          setButtonColor(newButtonColor);
-        }}
-      >
-        Change to {buttonColor}
-      </button>
-      <input
-        type="checkbox"
-        id="disable-button-checkbox"
-        defaultChecked={boxChecked}
-        onClick={() => {
-          setBoxChecked(!boxChecked);
-        }}
-      />
-      <label htmlFor="disable-button-checkbox">Disable button</label>
+    <div className="App">
+      <SummaryForm />
     </div>
   );
 }
